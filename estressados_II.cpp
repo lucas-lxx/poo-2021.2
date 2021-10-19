52ee6683ad6cfe7237f2dd817a264b9245e7d3d6
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-10-19 15:44:59 -0300

    added function vetor clonar de vetor referencia, second chapter

--- estressados_II.cpp
+++ estressados_II.cpp
@@ -5,5 +5,9 @@
 
 int main()
 {
+    std::vector<int> vetor_original = {1, 2, 3, 4, 5};
+    std::vector<int> vetor_copia = clonar(vetor_original);
+    std::cout << "O vetor original eh: " << vet_print(vetor_copia) << '\n';
+    std::cout << "O vetor copia eh: " << vet_print(vetor_copia) << '\n';
     
 }

9bd7a4a88638f2de8e79e3fb4ea6b586a70d40e6
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-10-19 06:59:38 -0300

    transfered the functions to the work-lib.hpp library

--- work.cpp
+++ work.cpp
@@ -1,49 +1,7 @@
 #include <iostream>
 #include <vector>
-
-// transforma funções booleanas de 0 e 1 para true ou false
-std::string to_alpha(bool x) {
-    return x ? "true" : "false";
-}
-
-// existe: Alguém com o valor X está na fila?
-bool existe (const std::vector<int>& fila, int x) {
-    for (int i : fila) {
-        if (x == i) 
-            return true;
-    }
-    return false;
-}
-
-// contar: Quantas vezes o valor X apareceu na fila?
-int contar(const std::vector<int>& fila, int x) {
-    int count = 0;
-    for (int i : fila) {
-        if (i == x) 
-            count++;
-    }
-    return count;
-}
-
-// procurar: Em que posição da fila aparece X pela primeira vez?
-// retorna -1 se não encontrar
-int procurar(const std::vector<int>& fila, int x){
-    for (int i : fila) {
-        if (i == x) 
-            return 1;
-    }
-    return -1;
-}
-
-// procurar_apartir: Dada a posição para iniciar a busca, qual a próxima posição em que aparece X?
-// retorna -1 se não encontrar
-int procurar_apartir(const std::vector<int> fila, int x, int inicio){
-    for (int i = inicio; i < (int) fila.size(); i++) {
-        if(fila[i] == x) 
-            return i;
-    }
-    return -1;
-}   
+#include "work-lib.hpp"
+   
 
 int main()
 {

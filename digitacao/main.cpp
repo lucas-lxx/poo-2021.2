58db6fa282f32a8a4dbcc10c1adca508dc5dab1a
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-11-22 19:55:36 -0300

    update window open/close

--- digitacao/main.cpp
+++ digitacao/main.cpp
@@ -2,5 +2,14 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Bubbles Type");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+    }
+    return 0;
 }

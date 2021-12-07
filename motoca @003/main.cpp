ac8133b3da9dd7bab164274ddaa9d759142ee404
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-07 19:29:52 -0300

    Added question template

--- motoca @003/main.cpp
+++ motoca @003/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
-int main() {
 
+
+struct Person {
+    int age;
+    std::string name;
+
+    Person(int age=0, std::string name=""):
+        age { age }, name { name } {
+    }
+};
+
+
+
+struct Motorcycle {
+    Person* person;
+    int power;
+    int time;
+
+    Motorcycle(Person* person=nullptr, int power=1, int time=0):
+        person { person }, power { power }, time { time } {
+    }
+
+    
+};
+
+
+
+int main() {
+    
 }

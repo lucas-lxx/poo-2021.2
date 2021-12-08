9bbe31bd364e5481c83b5118a34628920778ed11
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-08 09:58:46 -0300

    Overloaded operator <<

--- motoca @003/main.cpp
+++ motoca @003/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
-
-
 struct Person {
-    int age;
     std::string name;
+    int age;
 
-    Person(int age=0, std::string name=""):
-        age { age }, name { name } {
+    Person(std::string name="", int age=0):
+        name { name }, age { age } {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Person& person) {
+        os << person.name << " ";
+        os << person. age;
+        return os;
     }
 };
 
@@ -22,11 +26,33 @@ struct Motorcycle {
         person { person }, power { power }, time { time } {
     }
 
-    
+    // Returns true if there is someone on the motorcycle
+    // Else returns false and atributes someone to the motorcycle
+    bool in(Person* person) {
+        if (this->person == nullptr) {
+            this->person = person;
+            return false;
+        }
+        return true;
+    }   
+
+    friend std::ostream& operator<<(std::ostream& os, Motorcycle& motorcycle) {
+        os << "power: " << motorcycle.power << ", ";
+        os << "minutes: " << motorcycle.time << ", ";
+        if (motorcycle.in(motorcycle.person)) {
+            os << "person: " << motorcycle.person->name; 
+            return os;
+        }
+        os << "person: null"; 
+        return os;
+    }
 };
 
 
 
 int main() {
-    
+    Person person { "Bryan", 23 };
+    Motorcycle kx;
+    std::cout << kx << '\n';
+    std::cout << person << '\n';
 }

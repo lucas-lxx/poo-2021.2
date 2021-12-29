e7e69d8546faaff311e80d855437873f1a47571c
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-29 16:07:40 -0300

    updated operator<<() method

--- arcade/lapiseira @020/pencil.hpp
+++ arcade/lapiseira @020/pencil.hpp
@@ -6,24 +6,15 @@
 
 class Pencil {
 private:
-    Lead lead {"HB", 30, 0.3};
-    Lead lead2 {"H2", 40, 0.3};
-
-    std::list<Lead*> barrel {&lead, &lead2};
+    std::list<std::shared_ptr<Lead>> barrel{};
     float thickness { 0 };
-    Lead* tip { nullptr };
+    std::shared_ptr<Lead> tip { nullptr };
 
 public:
     Pencil(float thickness = 0.0) :
         thickness{thickness} {
     }
 
-    friend std::ostream& operator<<(std::ostream& os, std::list<Lead*> lead) {
-        for (Lead* i : lead) {
-            os << *i;
-        }
-        return os;
-    }
 
     friend std::ostream& operator<<(std::ostream& os, Pencil& pencil) {
         os << "thickness: " << pencil.thickness << ", ";
@@ -33,10 +24,9 @@ public:
             os << "tip: " << pencil.tip << ", ";
         }
         os << "barrel: {"; 
-        os << &pencil.barrel;
-        // for (Lead* i : pencil.barrel){
-        //     os << *i;
-        // }
+        for (auto i : pencil.barrel) {
+            os << *i;
+        }
         os << "}";
         return os;
     }

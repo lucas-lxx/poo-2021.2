2ebcc3b19359de6772d38db09ce567ee36d4ec04
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-29 18:55:26 -0300

    updated

--- arcade/lapiseira @020/main.cpp
+++ arcade/lapiseira @020/main.cpp
@@ -5,20 +5,26 @@
 #include "pencil.hpp"
 
 int main() {
-    std::shared_ptr<Lead> lead { new Lead {0.3, "4B", 30}};
-    std::shared_ptr<Lead> lead2 { new Lead {0.3, "HB", 80}};
+    std::shared_ptr<Lead> lead { new Lead {0.3, "4B", 11}};
+    std::shared_ptr<Lead> lead2 { new Lead {0.3, "2B", 80}};
 
     Pencil pencil {0.3};
-    std::cout << "insert():   " << pencil.insert(lead) << '\n';
-    std::cout << "show():     " << pencil << '\n';
 
-    std::cout << "insert():   " << pencil.insert(lead2) << '\n';
-    std::cout << "show():     " << pencil << '\n';
-    std::cout << "pull():     " << pencil.pull() << '\n';
-    std::cout << "show():     " << pencil << '\n';
-    std::cout << "remove():   " << *pencil.remove() << '\n';
-    std::cout << "show():     " << pencil << '\n';
+    pencil.insert(lead);
+    pencil.pull();
+    std::cout << pencil << '\n';
+    // std::cout << "insert():   " << pencil.insert(lead) << '\n';
+    // std::cout << "show():     " << pencil << '\n';
+
+    // std::cout << "insert():   " << pencil.insert(lead2) << '\n';
+    // std::cout << "show():     " << pencil << '\n';
+    // std::cout << "pull():     " << pencil.pull() << '\n';
+    // std::cout << "show():     " << pencil << '\n';
+    // std::cout << "remove():   " << *pencil.remove() << '\n';
+    // std::cout << "show():     " << pencil << '\n';
     
-    std::cout << "remove():   " << *pencil.remove() << '\n';
-    std::cout << "show():     " << pencil << '\n';
+    // std::cout << "remove():   " << *pencil.remove() << '\n';
+    // std::cout << "show():     " << pencil << '\n';
+    pencil.writePage();
+    std::cout << pencil << '\n';
 }

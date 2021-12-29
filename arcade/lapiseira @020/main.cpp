54fa00b6948235fd25e6151abd848b0e195a8a7e
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-29 17:41:19 -0300

    updated

--- arcade/lapiseira @020/main.cpp
+++ arcade/lapiseira @020/main.cpp
@@ -6,8 +6,14 @@
 
 int main() {
     std::shared_ptr<Lead> lead { new Lead {0.3, "4B", 30}};
+    std::shared_ptr<Lead> lead2 { new Lead {0.3, "HB", 80}};
 
     Pencil pencil {0.3};
-    std::cout << pencil.insert(lead) << '\n';
+    // std::cout << pencil.insert(lead) << '\n';
+    // std::cout << pencil << '\n';
+    // std::cout << pencil.insert(lead2) << '\n';
+    // std::cout << pencil << '\n';
+    
+    std::cout << pencil << '\n';
     std::cout << pencil << '\n';
 }

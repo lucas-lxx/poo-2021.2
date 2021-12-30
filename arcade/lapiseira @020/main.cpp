6a34b2737e64c8784d69b5ffb66b1b30ff009f4c
Lucas Almeida <lucas.lxx@alu.ufc.br>
2021-12-30 12:47:20 -0300

    updated

--- arcade/lapiseira @020/main.cpp
+++ arcade/lapiseira @020/main.cpp
@@ -5,26 +5,6 @@
 #include "pencil.hpp"
 
 int main() {
-    std::shared_ptr<Lead> lead { new Lead {0.3, "4B", 11}};
-    std::shared_ptr<Lead> lead2 { new Lead {0.3, "2B", 80}};
-
-    Pencil pencil {0.3};
-
-    pencil.insert(lead);
-    pencil.pull();
-    std::cout << pencil << '\n';
-    // std::cout << "insert():   " << pencil.insert(lead) << '\n';
-    // std::cout << "show():     " << pencil << '\n';
-
-    // std::cout << "insert():   " << pencil.insert(lead2) << '\n';
-    // std::cout << "show():     " << pencil << '\n';
-    // std::cout << "pull():     " << pencil.pull() << '\n';
-    // std::cout << "show():     " << pencil << '\n';
-    // std::cout << "remove():   " << *pencil.remove() << '\n';
-    // std::cout << "show():     " << pencil << '\n';
-    
-    // std::cout << "remove():   " << *pencil.remove() << '\n';
-    // std::cout << "show():     " << pencil << '\n';
-    pencil.writePage();
-    std::cout << pencil << '\n';
+    Pencil pencil;
+    pencil.case_identifier();
 }

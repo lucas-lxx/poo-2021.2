8908095467b8a5ba349cd8a133d63b75d3f6fc7d
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-02 14:51:19 -0300

    updated

--- arcade/cinema @010/main.cpp
+++ arcade/cinema @010/main.cpp
@@ -5,5 +5,11 @@
 #include "client.hpp"
 
 int main() {
-    
+    auto sclient = std::make_shared<Client>("Lucas", "3322");
+    Client client {"Lucas", "3324"};
+
+    std::cout << *sclient << '\n';
+    std::cout << client << '\n';
+    sclient->show();
+    client.show();
 }

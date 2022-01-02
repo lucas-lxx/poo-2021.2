815a1607143613e72f8bb1238b52b9d2bf377a3c
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-02 15:58:06 -0300

    added reservation() method

--- arcade/cinema @010/sala.hpp
+++ arcade/cinema @010/sala.hpp
@@ -10,41 +10,70 @@ class Sala {
 private:
     std::vector<std::shared_ptr<Client>> sala{};
     
+    // Returns true if the person already has a reservation
+    bool isPresent(const std::string& name) const {
+        for (std::shared_ptr<Client> i : sala) {
+            if (i != nullptr) {
+                if (i->getId() == name) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Returns true if the chair is available
+    bool isChairAvailable(int chair) {
+        if (chair < (int) this->sala.size()) {
+            if (this->sala[chair] == nullptr) {
+                return true;
+            } else {
+                std::cout << "fail: this chair is not available\n";
+                return false;
+            }
+        }
+        std::cout << "fail: there is no chair with this number\n";
+        return false;
+    }
+
 public:
     ///////////////////////////////////////////////////////////////////////////
     //////////////Constructor//////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    Sala() {
+    Sala(int capacity = 0) :
+        sala(capacity, nullptr) {
     }
 
     ///////////////////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////
+    ///////////////Methods/////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-
+    // Returns true if it was possible to make a reservation
+    bool reservation(std::string id, std::string fone, int chair) {
+        if (!isPresent(id)) {
+            if (isChairAvailable(chair)) {
+                this->sala[chair] = std::make_shared<Client>(id, fone);
+                return true;
+            }
+        }
+        return false;
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     //////////////Print Methods////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    friend std::ostream& operator<<(std::ostream& os, const Sala& sala) {
-        os << "[ ";
-        for (auto i : sala.sala) {
-            if (i == nullptr) {
-                os << "---- ";
-            } else {
-                os << i << ' ';
-            }
-        }
-        os << "]";
-        return os;
-    }
-
     void show() const {
+        int count{0};
         std::cout << "[ ";
         for (auto i : this->sala) {
-            std::cout << *i;
+            if (i == nullptr) {
+                std::cout << count << "---- ";
+            } else {
+                std::cout << *i << ' ';
+            }
+            count++;
         }
         std::cout << "]\n";
     }

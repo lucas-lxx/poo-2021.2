d565a13b688f2966c46865faa16741cd72207328
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-04 12:26:54 -0300

    updated

--- arcade/contato @014/main.cpp
+++ arcade/contato @014/main.cpp
@@ -3,7 +3,15 @@
 #include "fone.hpp"
 
 int main() {
-    Fone fone{"oi", "(85)22a01"};
-    std::cout << fone << '\n';
-    std::cout << fone.isValid() << '\n';
+    Fone fone{"oi", "(85)2201"};
+    Fone fone2{"tim", "(85)22.33"};
+    Fone fone3{"claro", "2244"};
+    Contact c{"Lucas"};
+
+    std::cout << "fone: " << c.addFone(fone) << '\n';
+    std::cout << "fone2: " <<  c.addFone(fone2) << '\n';
+    std::cout << "fone3: " <<  c.addFone(fone3) << '\n';
+    std::cout << "show: " << c << '\n';
+    std::cout << "remove: " << c.rmFone(1) << '\n';
+    std::cout << "show: " << c << '\n';
 }

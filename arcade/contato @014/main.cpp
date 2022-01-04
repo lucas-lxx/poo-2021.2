4bd97a735c86c9a4e2f5efc75296b7f88b6c1526
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-04 11:36:06 -0300

    updated

--- arcade/contato @014/main.cpp
+++ arcade/contato @014/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "contact.hpp"
+#include "fone.hpp"
 
 int main() {
-
+    Fone fone{"oi", "(85)22a01"};
+    std::cout << fone << '\n';
+    std::cout << fone.isValid() << '\n';
 }

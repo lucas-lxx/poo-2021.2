f104b40e1f9370c893aeb717d433324103dd157b
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-04 18:09:08 -0300

    updated

--- arcade/busca-@015/main.cpp
+++ arcade/busca-@015/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <memory>
+#include <vector>
+#include "../contato-@014/contact.hpp"
+#include "agenda.hpp"
 
 int main() {
-
+    Fone fone{"oi", "1234"};
+    Fone fone2{"tim", "0897"};
+    std::vector<Fone> fones{fone, fone2};
+    Fone fone12{"oi", "122"};
+    Fone fone22{"tim", "4334"};
+    Fone fone32{"claro", "232a3"};
+    std::vector<Fone> fones2{fone12, fone22, fone32};
+    Agenda agenda;
+    agenda.addContact("Lucas", fones);
+    agenda.addContact("Norma", fones2);
+    std::cout << agenda;
 }

cf8fe7f484f9d001d9cea0b6ae43bb285d11ed6a
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-04 18:08:42 -0300

    added addContact()

--- arcade/busca-@015/agenda.hpp
+++ arcade/busca-@015/agenda.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <iostream>
 #include <list>
+#include <vector>
 #include "../contato-@014/contact.hpp"
 
 class Agenda {
 private:
     std::list<Contact> contacts{};
 
-    std::list<Contact>::iterator findPosByName(std::string name) {
+    // std::list<Contact>::iterator findPosByName(std::string name) {
 
-    }
+    // }
 
 public:
     ///////////////////////////////////////////////////////////////////////////
@@ -23,7 +24,17 @@ public:
     ///////////////Methods/////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-
+    bool addContact(std::string name, std::vector<Fone> fones) {
+        Contact person{name};
+        for (auto i : fones) {
+            if (!person.addFone(i)) {
+                std::cout << "fail: " << name << '\n';
+                return false;
+            }
+        }
+        this->contacts.push_back(person);
+        return true;
+    }
 
     std::list<Contact> getContacts() {
         return this->contacts;

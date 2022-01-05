e15c28f78fe03b99a1d35299f4e237ac862795f3
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-05 15:30:21 -0300

    updated

--- arcade/busca-@015/main.cpp
+++ arcade/busca-@015/main.cpp
@@ -8,23 +8,4 @@
 int main() {
     Solver solver;
     solver.cases();
-    // Fone fone{"oi", "1234"};
-    // Fone fone2{"tim", "0897"};
-    // std::vector<Fone> fones{fone, fone2};
-    // Fone fone12{"oi", "122"};
-    // Fone fone22{"tim", "4334"};
-    // Fone fone32{"claro", "2323"};
-    // std::vector<Fone> fones2{fone12, fone22, fone32};
-    // Agenda agenda{};
-    // agenda.addContact("lucas", fones);
-    // agenda.addContact("norma", fones2);
-    // std::cout << agenda;
-    // std::cout << "----\n";
-    // Fone fone0{"zero", "0000"};
-    // std::vector<Fone>asdf{fone0};
-    // agenda.addContact("bia", asdf);
-    // std::cout << agenda;
-    // std::cout << "----\n";
-    // agenda.rmContact("Lucas");
-    // std::cout << agenda;
 }

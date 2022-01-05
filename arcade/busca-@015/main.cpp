d49cadf83d8115739cd56338f0dbdac1065212b6
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-05 12:07:10 -0300

    updated

--- arcade/busca-@015/main.cpp
+++ arcade/busca-@015/main.cpp
@@ -3,28 +3,31 @@
 #include <vector>
 #include "../contato-@014/contact.hpp"
 #include "agenda.hpp"
+#include "solver.hpp"
 
 int main() {
-    Fone fone{"oi", "1234"};
-    Fone fone2{"tim", "0897"};
-    std::vector<Fone> fones{fone, fone2};
-    Fone fone12{"oi", "122"};
-    Fone fone22{"tim", "4334"};
-    Fone fone32{"claro", "2323"};
-    std::vector<Fone> fones2{fone12, fone22, fone32};
-    Agenda agenda{{}};
-    agenda.addContact("Lucas", fones);
-    agenda.addContact("Norma", fones2);
-    std::cout << agenda;
-    std::cout << "----\n";
-    Fone fone0{"zero", "0000"};
-    std::vector<Fone>asdf{fone0};
-    agenda.addContact("Leh", asdf);
-    std::cout << agenda;
-    // std::cout << "----\n";
-    // agenda.rmContact("Lucas");
+    Solver solver;
+    solver.cases();
+    // Fone fone{"oi", "1234"};
+    // Fone fone2{"tim", "0897"};
+    // std::vector<Fone> fones{fone, fone2};
+    // Fone fone12{"oi", "122"};
+    // Fone fone22{"tim", "4334"};
+    // Fone fone32{"claro", "2323"};
+    // std::vector<Fone> fones2{fone12, fone22, fone32};
+    // Agenda agenda{{}};
+    // agenda.addContact("Lucas", fones);
+    // agenda.addContact("Norma", fones2);
     // std::cout << agenda;
-    std::cout << "F----\n";
-    Agenda found {agenda.search("L")};
-    std::cout << found;
+    // std::cout << "----\n";
+    // Fone fone0{"zero", "0000"};
+    // std::vector<Fone>asdf{fone0};
+    // agenda.addContact("Leh", asdf);
+    // std::cout << agenda;
+    // // std::cout << "----\n";
+    // // agenda.rmContact("Lucas");
+    // // std::cout << agenda;
+    // std::cout << "F----\n";
+    // Agenda found {agenda.search("000")};
+    // std::cout << found;
 }

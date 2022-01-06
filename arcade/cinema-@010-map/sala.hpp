de5f41494cc7ea381b301c94fef9d119ca6903b2
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-06 11:46:33 -0300

    updated isChairAvailable()

--- arcade/cinema-@010-map/sala.hpp
+++ arcade/cinema-@010-map/sala.hpp
@@ -4,28 +4,31 @@
 #include <vector>
 #include <sstream>
 #include <memory>
+#include <unordered_set>
 #include <unordered_map>
 #include "client.hpp"
 
 
 class Sala {
 private:
-    std::unordered_map<std::string, std::shared_ptr<Client>> sala;
+    std::unordered_map<std::string, std::shared_ptr<Client>> customers;
+    std::unordered_map<int, std::string> chairs;
+    int cinema_size = {0};
     
     // Returns the iterator of the map if the person is present
-    // If there is no one present that matches, then returns this->sala.end()
+    // If there is no one present that matches, then returns this->customers.end()
     auto isPresent(const std::string& name) {
-        auto it = this->sala.find(name);
-        if (it != this->sala.end()) {
+        auto it = this->customers.find(name);
+        if (it != this->customers.end()) {
             return it;
         }
-        return this->sala.end();
+        return this->customers.end();
     }
 
     // Returns true if the chair is available
     bool isChairAvailable(int chair) {
-        if (chair < (int) getCadeiras().size()) {
-            if (getCadeiras()[chair] == nullptr) {
+        if (chair < cinema_size) {
+            if (this->chairs.find(chair) == this->chairs.end()) {
                 return true;
             } else {
                 std::cout << "fail: this chair is not available\n";
@@ -41,8 +44,9 @@ public:
     //////////////Constructor//////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    Sala(int capacity = 0) {
-        this->sala.reserve(capacity);
+    Sala(int capacity = 0) :
+        cinema_size(capacity) {
+        this->customers.reserve(capacity);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -61,7 +65,7 @@ public:
 
     // Returns true if it was possible to make a reservation
     bool reservation(std::string id, std::string fone, int chair) {
-        if (isPresent(id) == -1) {
+        if (isPresent(id) == this->customers.end()) {
             if (isChairAvailable(chair)) {
                 getCadeiras()[chair] = std::make_shared<Client>(id, fone);
                 return true;
@@ -74,7 +78,7 @@ public:
 
     // Returns the vector with the cinema's chairs
     std::unordered_map<std::string, std::shared_ptr<Client>>& getCadeiras() {
-        return this->sala;
+        return this->customers;
     }
 
 
@@ -104,7 +108,7 @@ public:
     void case_identifier() {
         std::string line{""};
         std::string command{""};
-        std::shared_ptr<Sala> sala = std::make_shared<Sala>();
+        std::shared_ptr<Sala> customers = std::make_shared<Sala>();
         while(true) {
             line = "";
             command = "";
@@ -117,9 +121,9 @@ public:
             } else if (command == "init") {
                 int size;
                 ss >> size;
-                sala = std::make_shared<Sala>(size);
+                customers = std::make_shared<Sala>(size);
             } else if (command == "show") {
-                sala->show();
+                customers->show();
             } else if (command == "reservation") {
                 std::string name{"INV"};
                 std::string fone{"INV"};
@@ -135,11 +139,11 @@ public:
                     std::cout << "Chair input invalid!\n";
                     continue;
                 }
-                sala->reservation(name, fone, chair);
+                customers->reservation(name, fone, chair);
             } else if (command == "cancel") {
                 std::string name;
                 ss >> name;
-                sala->cancel(name);
+                customers->cancel(name);
             } else if (command == "help") {
                 std::cout << "init <number of chairs>: initializes the chairs of the cinema\n";
                 std::cout << "show: shows all available chairs\n";

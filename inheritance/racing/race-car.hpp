7d408f69e2de363affc6d6e7c55be7207a0d9b3b
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-06 20:52:22 -0300

    added speed()

--- inheritance/racing/race-car.hpp
+++ inheritance/racing/race-car.hpp
@@ -9,9 +9,15 @@ private:
     Tyre tyreCompound{Tyre::null};
     bool turbo{true};
 public:
-    RaceCar(int aeroDynamicPressure = 0, Tyre tyre = Tyre::null, bool turbo=true, int passengers = 1, int power = 10):
-        aeroDynamicPressure{aeroDynamicPressure}, tyreCompound{tyre}, turbo{turbo}, Car{passengers, power} {
+    RaceCar(int aeroDynamicPressure = 0, Tyre tyre = Tyre::null, bool turbo=true, std::shared_ptr<Driver> driver = nullptr, int passengers = 1, int power = 10):
+        aeroDynamicPressure{aeroDynamicPressure}, tyreCompound{tyre}, turbo{turbo}, Car{driver, passengers, power} {
     }
 
+    virtual int speed() const {
+        auto driver = Car::getDriver();
+        if (driver == nullptr)
+            return Car::getPower() / Car::getPassengers();
+        return (Car::getPower() / Car::getPassengers()) * driver->getSkill();
+    }
 
 };

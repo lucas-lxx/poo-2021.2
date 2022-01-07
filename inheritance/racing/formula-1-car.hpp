981e001088770d7131b5a89ab341d2bbe3edf57d
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-07 13:54:59 -0300

    added enum class Scuderia, added functions to construct based on the Scuderia

--- inheritance/racing/formula-1-car.hpp
+++ inheritance/racing/formula-1-car.hpp
@@ -1,7 +1,60 @@
 #pragma once
 #include <iostream>
+#include <random>
+#include <chrono>
 #include "race-car.hpp"
 
-class Formula_1 : public RaceCar {
+enum class Scuderia {
+    null,
+    ferrari,
+    mercedes,
+    red_bull,
+    mclaren,
+    williams
+};
+
+int scuderiaPower(Scuderia scuderia = Scuderia::null) {
+    switch (scuderia){
+        case (Scuderia::null):
+            return 0;
+        case (Scuderia::ferrari):
+            return 825;
+        case (Scuderia::mercedes):
+            return 850;
+        case (Scuderia::red_bull):
+            return 840;
+        case (Scuderia::mclaren):
+            return 820;
+        case (Scuderia::williams):
+            return 800;
+    }
+}
+
+int scuderiaAeroDynamic(Scuderia scuderia = Scuderia::null) {
+    switch (scuderia){
+        case (Scuderia::null):
+            return 0;
+        case (Scuderia::ferrari):
+            return 80;
+        case (Scuderia::mercedes):
+            return 90;
+        case (Scuderia::red_bull):
+            return 100;
+        case (Scuderia::mclaren):
+            return 85;
+        case (Scuderia::williams):
+            return 80;
+    }
+}
+
+class FormulaOne : public RaceCar {
+private:
+    Scuderia scuderia{Scuderia::null};
+
+public:
+
+    FormulaOne(Tyre tyre = Tyre::null, Scuderia scuderia = Scuderia::null) :
+        RaceCar{scuderiaPower(scuderia), scuderiaAeroDynamic(scuderia), tyre} {
+    }
 
 };

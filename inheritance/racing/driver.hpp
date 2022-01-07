6f50cf4953dd05b82ffdc9cfbf51359c05f0d3a9
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-07 14:47:05 -0300

    added driving variable

--- inheritance/racing/driver.hpp
+++ inheritance/racing/driver.hpp
@@ -5,14 +5,23 @@
 class Driver : public Person {
 private:
     int driverSkill{0};
+    bool driving{false};
 
 public:
     Driver(std::string name = "", int age = 0, int skill = 0):
-        Person{age, name}, driverSkill(skill) {
+        Person{age, name}, driverSkill(skill), driving{false} {
     }
 
     virtual int getSkill() {
         return driverSkill;
+    }
+
+    virtual bool isDriving() {
+        return driving;
+    }
+
+    virtual void setDriving(bool driving) {
+        this->driving = driving;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Driver& driver) {

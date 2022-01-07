59b55a4ec6c03168ffe58635ee4636a61593370d
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-07 13:49:51 -0300

    isIt() updated to check drivers and addDriver() added

--- inheritance/racing/car.hpp
+++ inheritance/racing/car.hpp
@@ -5,6 +5,7 @@
 
 class Car {
 private:
+    std::string type{"Car"};
     std::shared_ptr<Driver> driver{nullptr};
     int passengers{4};
     int power{150};
@@ -15,17 +16,21 @@ public:
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    Car(std::shared_ptr<Driver> driver = nullptr, int passengers = 4, int power = 150) :
-        driver{driver}, passengers{passengers}, power{power} {
-    }
-
-    virtual ~Car() {
-        std::cout << "The car got to the junkyard\n";
+    Car(int passengers = 4, int power = 150) :
+        type{"Car"}, driver{nullptr}, passengers{passengers}, power{power} {
     }
 
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
+
+    virtual bool addDriver(std::shared_ptr<Driver> driver) {
+        if (this->driver == nullptr) {
+            this->driver = driver;
+            return true;
+        }
+        return false;
+    }
 
     virtual int speed() const {
         if (driver == nullptr)
@@ -35,7 +40,7 @@ public:
 
     virtual void race(const Car& car) {
         if (this->speed() > car.speed()) {
-            std::cout << "My duck! Quaqua\n";
+            std::cout << "My duck! Quack quack!\n";
             myDucks++;
         } else {
             std::cout << "Oh snap I got smoked\n";
@@ -43,11 +48,15 @@ public:
     }
 
     virtual void isItMax() {
-        std::cout << "https://www.youtube.com/watch?v=43HCYSXZ9GI&t=104s <- click it!\n";
+        if (driver->getName() == "max" || driver->getName() == "Max") {
+            std::cout << "https://www.youtube.com/watch?v=43HCYSXZ9GI&t=104s <- click it!\n";
+        }
     }
 
     virtual void isItGlockGoingSlowly() {
-        std::cout << "https://www.youtube.com/watch?v=RAICZZVoZI8&t=19s <- click it!\n";
+        if (driver->getName() == "hamilton" || driver->getName() == "Hamilton") {
+            std::cout << "https://www.youtube.com/watch?v=RAICZZVoZI8&t=19s <- click it!\n";
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -70,6 +79,10 @@ public:
         return passengers;
     }
 
+    virtual std::string getType() const {
+        return type;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
@@ -78,12 +91,16 @@ public:
         this->myDucks = ducks;
     }
 
+    virtual void setType(std::string new_type) {
+        this->type = new_type;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
     friend std::ostream& operator<<(std::ostream& os, const Car& car) {
-        os << "Car: " << car.power << " duck counter: " << car.myDucks;
+        os << car.type << " power: " << car.power << ", Driver: " << car.driver->getName();
         return os;
     }
 };

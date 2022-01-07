b368884b0a7d77290f1e5cabc7542903fad40607
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-07 16:10:01 -0300

    updated

--- inheritance/racing/main.cpp
+++ inheritance/racing/main.cpp
@@ -8,27 +8,6 @@
 #include "formula-1-car.hpp"
 
 int main() {
-    // std::shared_ptr<Driver> max = std::make_shared<Driver>("Max", 24, 99);
-    // std::cout << (*max) << '\n';
-
-    // Car car(4, 100);
-    // car.addDriver(max);
-    // std::cout << "car " << car.speed() << '\n';
-
-    // RaceCar race_car(450, 10, Tyre::medium);
-    // race_car.addDriver(max);
-
-    // FormulaOne formula_1_car(Tyre::soft, Scuderia::mclaren);
-    // formula_1_car.addDriver(max);
-
-    // std::cout << car << '\n';
-    // std::cout << race_car << '\n';
-    // std::cout << formula_1_car << '\n';
-    // car.race(formula_1_car);
-    // std::cout << car.speed() << " " << race_car.speed() << " " << formula_1_car.speed() << '\n'; 
-    // std::cout << formula_1_car.developPower() << '\n';
-    // std::cout << formula_1_car.developAeroDynamic() << '\n';
-    // std::cout << car.speed() << " " << race_car.speed() << " " << formula_1_car.speed() << '\n'; 
     auto max = std::make_shared<Driver>("Max", 24, 99);
     auto lewis = std::make_shared<Driver>("Sir Lewis", 37, 95);
 

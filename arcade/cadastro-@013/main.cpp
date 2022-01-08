04b80585f2456541fde77f6b6c601f5c386d7acc
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-08 19:00:48 -0300

    updated

--- arcade/cadastro-@013/main.cpp
+++ arcade/cadastro-@013/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include "account.hpp"
+#include "customer.hpp"
 #include "bank-agency.hpp"
 
 // Prints two accounts to the terminal
-void showAccounts(Account& savings_account, Account& checking_account) {
-    std::cout << "savings: " << savings_account << "\nchecking: " << checking_account << '\n';
-}
+// void showAccounts(Account& savings_account, Account& checking_account) {
+//     std::cout << "savings: " << savings_account << "\nchecking: " << checking_account << '\n';
+// }
 
 int main() {
-    SavingsAccount savings_account{0, "Lucas"};
-    CheckingAccount checking_account{1, "Lucas"};
+    // SavingsAccount savings_account{0, "Lucas"};
+    // CheckingAccount checking_account{1, "Lucas"};
 
-    showAccounts(savings_account, checking_account);
-    savings_account.monthlyUpdate();
-    checking_account.monthlyUpdate();
-    std::cout << "-----\n";
-    showAccounts(savings_account, checking_account);
+    // showAccounts(savings_account, checking_account);
+    // savings_account.monthlyUpdate();
+    // checking_account.monthlyUpdate();
+    // std::cout << "-----\n";
+    // showAccounts(savings_account, checking_account);
+    Customer lucas;
+    std::cout << lucas << '\n';
 }

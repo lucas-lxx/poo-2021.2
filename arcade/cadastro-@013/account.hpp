4afc7dfcc28dd63069e68d78bb995bf19e253d04
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-08 13:58:50 -0300

    added Customer attributes

--- arcade/cadastro-@013/account.hpp
+++ arcade/cadastro-@013/account.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include <unordered_map>
 
 class Customer {
+private:
+    std::map<std::string, Account> accounts;
+    std::string customerId{""};
+
+public:
+    Customer(std::string customerId = ""): 
+        customerId{customerId} {
+    }
+
+    void addAccount(Account* account) {
+        // accounts[account->getAccountId()] = account;
+    }
 
 };
 
 class Account {
+private:
+    std::string accountId{""};
 
+public:
+    Account(std::string account = ""):
+        accountId(account) {
+    }
 };
 
 class SavingsAccount : public Account {

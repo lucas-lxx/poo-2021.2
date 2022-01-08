e4e36e96d8b515047121dadd9a88597109d58eb9
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-08 12:11:16 -0300

    up

---
+++ arcade/cadastro-@013/account.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include "customer.hpp"
+
+class Account {
+
+};
+
+class SavingsAccount : public Account {
+
+};
+
+class CheckingAccount : public Account {
+
+};
---
+++ arcade/cadastro-@013/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "account.hpp"
+#include "bank-agency.hpp"
+#include "customer.hpp"
+
+int main() {
+
+}

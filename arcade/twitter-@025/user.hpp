4e9b6763fde2025dbb11ebb286d33beb1893eb10
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-15 18:31:05 -0300

    added follow() and destructor

--- arcade/twitter-@025/user.hpp
+++ arcade/twitter-@025/user.hpp
@@ -5,8 +5,37 @@
 
 
 class User {
+private:
     std::map<std::string, User*> followers;
     std::map<std::string, User*> following;
-    Inbox inbox;
+    Inbox* inbox;
+    std::string username;
+
+public:
+
+    User(std::string username = ""): 
+        username{username} {
+        this->inbox = nullptr;
+    }
+
+    ~User() {
+        delete inbox;
+        for (auto i : followers) {
+            delete i.second;
+        }
+        for (auto i : following) {
+            delete i.second;
+        }
+    }
+
+    void follow(User& other) {
+        if (other.followers.find(username) == other.followers.end()) {
+            other.followers[username] = this;
+            this->following[other.username] = &other;
+        }
+        throw std::runtime_error("fail: user not found");
+    }
+
+
     
 };

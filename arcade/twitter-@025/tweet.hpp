d059aa99ea5ec21afda38c8a9ae18529aae3dee6
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-15 18:54:07 -0300

    added constructor

--- arcade/twitter-@025/tweet.hpp
+++ arcade/twitter-@025/tweet.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include <set>
 
 class Tweet {
 private:
-    
+    int id;
+    std::set<std::string> likes;
+    std::string username;
+    std::string msg;
+
+public:
+
+    Tweet(int id = -1, std::string username = "", std::string msg = ""): 
+        id{id}, username{username}, msg{msg} {
+    }
+
 };

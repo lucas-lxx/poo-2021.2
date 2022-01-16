142f5ec957012f7a41d453027f297ea3f25dafdb
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-16 11:32:56 -0300

    updated from cache to redundency

--- arcade/twitter-@025/inbox.hpp
+++ arcade/twitter-@025/inbox.hpp
@@ -3,46 +3,50 @@
 #include <map>
 #include <vector>
 #include <memory>
+#include <utility>
 #include "tweet.hpp"
 #include "../../cpp-functional/auxiliar.hpp"
 
 class Inbox {
 private:
-    std::vector<std::shared_ptr<Tweet>> all_tweets;
-    int first_unread;
-    bool read;
+    std::map<int, std::shared_ptr<Tweet>> all_tweets;
+    std::map<int, std::shared_ptr<Tweet>> unread;
 
 public:
-    Inbox() {
-        this->first_unread = 0;
-        this->read = false;
-    }
+    Inbox() {}
 
-    std::vector<std::shared_ptr<Tweet>> get_all() {
+    std::map<int, std::shared_ptr<Tweet>> get_all() {
         return this->all_tweets;
     }
 
-    Tweet get_tweet(int id) {
-        return all_tweets[id]->getId();
+    std::shared_ptr<Tweet> get_tweet(int id) {
+        auto it = all_tweets.find(id);
+        if (it != all_tweets.end()) {
+            return it->second;
+        }
+        throw std::runtime_error("fail: tweet not found!");
     }
 
-    std::vector<std::shared_ptr<Tweet>> get_unread() {
-        static std::vector<std::shared_ptr<Tweet>> unreaded_tweets;
-        if (!read) {
-            unreaded_tweets.clear();
-            unreaded_tweets = aux::slice(all_tweets, first_unread);
-            first_unread = all_tweets.size();
+    std::vector<Tweet> get_unread() {
+        std::vector<Tweet> unread_copy;
+        for (auto i : this->unread) {
+            unread_copy.push_back(*i.second);
         }
-        return unreaded_tweets;
+        unread.clear();
+        return unread_copy;
     }
 
     void new_tweet(std::shared_ptr<Tweet> tweet) {
-        all_tweets.push_back(tweet);
+        if (all_tweets.find(tweet->getId()) == all_tweets.end()) {
+            all_tweets.insert({tweet->getId(), tweet});
+            unread.insert({tweet->getId(), tweet});
+            return;
+        }
     }
 
     friend std::ostream& operator<<(std::ostream& os, Inbox inbox) {
         for (auto i : inbox.get_unread()) {
-            os << *i << '\n';
+            os << i << '\n';
         }
         return os;
     }

292f80f6c8a74dfa1465b1ad69efe0c0474be6ae
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-01-16 16:56:36 -0300

    added operator<<()

--- arcade/twitter-@025/user.hpp
+++ arcade/twitter-@025/user.hpp
@@ -4,11 +4,11 @@
 #include "inbox.hpp"
 
 
-class User {
+class User : std::enable_shared_from_this<User> {
 private:
-    std::map<std::string, User*> followers;
-    std::map<std::string, User*> following;
-    Inbox* inbox;
+    std::map<std::string, std::shared_ptr<User>> followers;
+    std::map<std::string, std::shared_ptr<User>> following;
+    std::shared_ptr<Inbox> inbox;
     std::string username;
 
 public:
@@ -18,14 +18,40 @@ public:
         this->inbox = nullptr;
     }
 
-    void follow(User& other) {
-        if (other.followers.find(username) == other.followers.end()) {
-            other.followers[username] = this;
-            this->following[other.username] = &other;
+    
+    void follow(std::shared_ptr<User> other) {
+        auto it = other->followers.find(username);
+        if (it == other->followers.end()) {
+            other->followers[username] = shared_from_this();
+            this->following[other->username] = other;
         }
         throw std::runtime_error("fail: user not found");
     }
 
+    friend std::ostream& operator<<(std::ostream& os, User user) {
+        os << user.username << '\n';
+        os << " following [";
+        int count {1};
+        for (auto i : user.following) {
+            if (count >= (int) user.following.size()) {
+                os << i.second;
+                count = 1;
+                break;
+            }
+            os << i.second << ", ";
+        }
+        os << "]\n";
 
+        os << " followers [";
+        for (auto i : user.followers) {
+            if (count >= (int) user.following.size()) {
+                os << i.second;
+                break;
+            }
+            os << i.second << ", ";
+        }
+        os << "]\n";
+        return os;
+    }
     
 };

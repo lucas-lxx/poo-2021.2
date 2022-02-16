b738bcb070bcaf60c080d5f04af4e885c06d0de1
Lucas Almeida <lucas.lxx@alu.ufc.br>
2022-02-16 14:26:54 -0300

    added vincular()

--- 15_hospital/hospital.hpp
+++ 15_hospital/hospital.hpp
@@ -41,10 +41,41 @@ public:
     }
 
     void vincular(std::string id_medico, std::string id_paciente) {
-        
+        auto medico = get_medico_iterator(id_medico)->second;
+        auto paciente = get_paciente_iterator(id_paciente)->second;
+        check_pacients_list(medico, id_paciente);
+        check_doctors_list(paciente, medico);
+        medico->add_paciente(paciente);
     }
 
 private:
+    void check_pacients_list(IMedico* medico, std::string id_paciente) {
+        auto check = medico->get_pacientes().find(id_paciente);
+        if (check != medico->get_pacientes().end())
+            throw std::runtime_error("fail: pacient already on the list");
+    }
+
+    void check_doctors_list(IPaciente* paciente, IMedico* medico) {
+        for (auto it_medico:paciente->get_medicos()){
+            if (it_medico.second->get_classe() == medico->get_classe())
+                throw std::runtime_error("fail: there is already a doctor with that expertise on atendece");
+        }
+    }
+
+    std::map<std::string, IMedico*>::iterator get_medico_iterator(std::string id_medico) {
+        auto it_medico = medicos.find(id_medico);
+        if (it_medico == medicos.end())
+            throw std::runtime_error("fail: doctor not found");
+        return it_medico;
+    }
+
+    std::map<std::string, IPaciente*>::iterator get_paciente_iterator(std::string id_paciente) {
+        auto it_paciente = pacientes.find(id_paciente);
+        if (it_paciente == pacientes.end())
+            throw std::runtime_error("fail: pacient not found");
+        return it_paciente;
+    }
+
     void remover_todos_os_pacientes(IMedico* medico) {
         for (auto [key, pac] : medico->get_pacientes())
             medico->remover_paciente(key);
--- 15_hospital/main.cpp
+++ 15_hospital/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include "paciente.hpp"
 #include "medico.hpp"
+#include "hospital.hpp"
 
 int main() {
+    Hospital hospital;
     auto p1 = new Paciente("L", "avc");
     auto m1 = new Medico("J", "asdf");
     auto m2 = new Medico("K", "asdf");
     std::cout << *p1 << '\n';
     try {
-        p1->add_medico(m1);
+        hospital.add_medico(m1);
+        hospital.add_medico(m2);
+        hospital.add_paciente(p1);
+        hospital.vincular(m1->get_id(), p1->get_id());
+        std::cout << *m1 << '\n';
         std::cout << *p1 << '\n';
-        p1->remover_medico(m1->get_id());
-        std::cout << *p1 << '\n';
+        hospital.vincular(m2->get_id(), p1->get_id());
 
     } catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
 
     
-    // std::cout << *m2 << '\n';
+    std::cout << *m1 << '\n';
     std::cout << *p1 << '\n';
     delete m1;
     delete m2;
